9c8eefc434b3ff7618557f7a57cdc560a25f840d
Tiberiu <musattiberiu@gmail.com>
2024-02-22 23:41:52 +0100

    judete legacy pages

--- src/app/(legacy)/info-judete/page.tsx
+++ src/app/(legacy)/info-judete/page.tsx
@@ -1,19 +1,5 @@
-import { Suspense } from "react";
-import { DynamicRedirect } from "~/components/DynamicRedirect";
-import { JUDETE } from "~/data/coduriJudete";
-import { ultimulAnBac } from "~/data/dbQuery";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <Suspense>
-      <DynamicRedirect
-        paramName="name"
-        data={JUDETE.map((judet) => ({
-          value: judet.numeIntreg,
-          redirect: `/judet/${judet.nume}`,
-        }))}
-        fallback={`/top_judete/${ultimulAnBac}`}
-      />
-    </Suspense>
-  );
+  redirect("/");
 }
