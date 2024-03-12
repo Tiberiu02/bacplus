cda69634306fe663a781efcd015d74258985be61
Tiberiu <musattiberiu@gmail.com>
2024-03-12 18:09:49 +0100

    fix

--- infra/cicd/server.ts
+++ infra/cicd/server.ts
@@ -49,12 +49,8 @@ server.post("/pull-and-deploy", async (req, res) => {
     await execCmd("git", ["pull"]);
     await execCmd("npm", ["install"]);
     // await execCmd("npm", ["run", "build"]);
-    await execCmd("npx", ["ts-node", "infra/cdn/bunny.ts"]);
-    await execCmd("npx", [
-      "ts-node",
-      "infra/backend/deploy.ts",
-      "bacplus-test",
-    ]);
+    await execCmd("npx", ["ts-node", "infra/cdn/bunny.ts", "bacplus-test"]);
+    await execCmd("npx", ["ts-node", "infra/backend/deploy.ts"]);
   } else {
     console.log(new Date(), "Invalid key");
     res.status(403).send("Invalid key");
