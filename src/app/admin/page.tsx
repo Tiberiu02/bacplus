45d3989e31429bdec505304091209cf957edf535
Tiberiu <musattiberiu@gmail.com>
2024-03-13 21:53:56 +0100

    db fix

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import { MainContainer } from "~/components/MainContainer";
+import { Title } from "~/components/Title";
+
+export default function Admin() {
+  return (
+    <MainContainer>
+      <Title>Admin</Title>
+      <p>Admin page</p>
+    </MainContainer>
+  );
+}
