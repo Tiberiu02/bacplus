758b7fdcf987a5127c3cc8b6bdad39a22d061867
Tiberiu <musattiberiu@gmail.com>
2024-06-20 11:58:15 +0200

    navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -66,7 +66,7 @@ export function Navbar({
     >
       <div
         className={twMerge(
-          "flex w-full max-w-6xl flex-col bg-transparent px-5 py-5",
+          "flex w-full max-w-6xl flex-col bg-transparent px-5 py-4",
           isOverlay && "py-4"
         )}
       >
